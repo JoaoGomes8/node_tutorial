c21a24110e4a431b83892db6dcbdb5d27928f5ea
JoaoGomes8 <joaogomes2609@gmail.com>
2023-02-03 19:09:41 +0000

    builtin modules

---
+++ 04_builtin_modules/main.js
@@ -0,0 +1,8 @@
+const os = require('os')
+
+// console.log(os);
+// console.log(os.version());
+// console.log(os.cpus()[0].speed);
+// console.log(os.homedir());
+// console.log(os.freemem());
+console.log(os.machine());
