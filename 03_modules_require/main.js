b28d6c5dcb49a1e793c89e3c9270bdf8cd2ad40e
JoaoGomes8 <joaogomes2609@gmail.com>
2023-02-03 17:55:07 +0000

    require, module.exports

---
+++ 03_modules_require/main.js
@@ -0,0 +1,10 @@
+// módulos e require
+console.clear();
+console.log('Inicio');
+
+const { nomes, cidades, texto, adicionar } = require('./modulo')
+
+console.log(nomes);
+console.log(cidades);
+console.log(texto);
+console.log(adicionar(10,20));
---
+++ 03_modules_require/modulo.js
@@ -0,0 +1,9 @@
+const nomes = ['joao','ana','carlos']
+const cidades = ['lisboa','porto','faro']
+const texto = "Olá mundo!"
+
+function adicionar(a,b) {
+    return a+b
+}
+
+module.exports = {nomes, cidades, texto, adicionar}
